a32231c431e45e75618720aa358ee6549565363d
kiranpe <36789502+kiranpe@users.noreply.github.com>
2021-10-12 23:41:29 +0530

    Update update_file.sh

--- update/update_file.sh
+++ update/update_file.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-new_end=$(tail -1 opc-ipv6-prefix.cfg | awk -F"," '{print $1}' | sed -n 1p | sed 's/\//\\\//g')
-new_start=$(tail -1 opc-ipv6-prefix.cfg | awk -F"," '{print $2}' | sed -n 1p | sed 's/ ^*//g' | sed 's/\//\\\//g')
+new_end=$(tail -1 ipv6-prefix.cfg | awk -F"," '{print $1}' | sed -n 1p | sed 's/\//\\\//g')
+new_start=$(tail -1 ipv6-prefix.cfg | awk -F"," '{print $2}' | sed -n 1p | sed 's/ ^*//g' | sed 's/\//\\\//g')
 
 exist_end=$(sed -n 549p xz.yaml | awk -F": " '{print $2}' | sed 's/\//\\\//g')
 exist_start=$(sed -n 550p xz.yaml | awk -F": " '{print $2}' | sed 's/\//\\\//g')
