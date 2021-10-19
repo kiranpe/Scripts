5f9b0d7ed2bcd6157d2b062966d7327173c562da
kiranpe <kiranwls2011@gmail.com>
2021-10-19 09:39:45 +0530

    script to check nginx status

---
+++ nginx_status/check_nginx_status.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+status=$(ps -ef | grep -i [n]ginx | grep master)
+
+if [ $? = "0" ];then
+  echo "Nginx is up and running.." > /dev/null 2>&1
+else
+  echo "Nginx is Down on `hostname`.. Starting Nginx Web Server" | mailx -s "Nginx status on `hostname`" kiranpeddineni@gmail.com 
+  sudo systemctl start nginx > /dev/null 2>&1
+  new_status=$(ps -ef | grep -i [n]ginx | grep master)
+    if [ -z "$new_status" ];then
+      echo "Nginx is still Down.. Check configuration once.." | mailx -s "Nginx status on `hostname`" kiranpeddineni@gmail.com
+    else
+      echo "Nginx is Up and Running Now.." > /dev/null 2>&1
+fi fi
